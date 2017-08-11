db06133e323a124e011f22a98956d44f87d37864
Dmitry Ptushkin <dbl6jl@gmail.com>
2017-08-11 18:06:34 +0300

    initial commit: downloaded and changed a bit react-hot-boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# human.io clone
+
+My test implementation of human.io with react and backbone
---
+++ src/containers/Root.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Root = () => (
+  <div>Hello react and backbone!</div>
+);
+
+export default Root
