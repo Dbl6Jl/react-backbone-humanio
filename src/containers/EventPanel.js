96b5e2e2262ef577f350d3404cd627794b47a9e2
Dmitry Ptushkin <dbl6jl@gmail.com>
2017-08-12 16:42:42 +0300

    15 mins events panel added

---
+++ src/containers/EventPanel.js
@@ -0,0 +1,30 @@
+/**
+ * Created by dsptushkin on 12.08.17.
+ */
+import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
+export default class EventPanel extends Component {
+
+  render() {
+    const {events} = this.props;
+    const list = events.map((el, i) => (
+      <li key={el.text + el.i}>
+        {el.date}
+        <br/>
+        {el.text}
+      </li>
+    ));
+    return (
+      <div>
+        <Button bsStyle="success">add event</Button>
+        <ul>
+          {list}
+        </ul>
+        <Button bsStyle="warning">delete human</Button>
+      </div>
+    );
+  }
+}
+EventPanel.defaultProps = {
+  events: [{text: 'testEvent', date: 'today'}]
+};
--- src/containers/Page.js
+++ src/containers/Page.js
@@ -2,9 +2,10 @@
  * Created by dsptushkin on 12.08.17.
  */
 import React, {Component} from 'react';
-import {Button, Col, Grid, Row} from 'react-bootstrap';
+import {Col, Grid, Row} from 'react-bootstrap';
 import Sidebar from './Sidebar';
 import HumanInfoForm from './HumanInfoForm';
+import EventPanel from './EventPanel';
 
 export default class Page extends Component {
   render() {
@@ -13,7 +14,7 @@ export default class Page extends Component {
         <Row className="show-grid">
           <Col xs={6} md={4}><Sidebar /></Col>
           <Col xs={6} md={4}><HumanInfoForm /></Col>
-          <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+          <Col xsHidden md={4}><EventPanel /></Col>
         </Row>
       </Grid>
     );
